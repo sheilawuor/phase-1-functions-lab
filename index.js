152a059f554eb61d86a811441266f4def44e4eb7
sheilawuor <sheilawuor01@gmail>
2025-09-17 15:51:06 +0300

    add changes

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks(distance) {
-    const hq = 42;
-    return Math.abs(distance - hq);
-    //returns the number of blocks given a value
-  }
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue - 42);
+}
 
-    function distanceFromHqInFeet(street){
-        return distanceFromHqInBlocks(street) * 264;
-        //returns the number of feet given a value
-    }
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+}
 
-    function distanceTravelledInFeet(start, destination){
-        return Math.abs(destination - start)*264
-    }
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264;
+}
 
-   function calculatesFarePrice(start, destination) {
-    const distance = distanceTravelledInFeet(start, destination);
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+
     if (distance <= 400) {
-        return 0;
+        return 0; 
     } else if (distance > 400 && distance <= 2000) {
-        return (distance - 400) * 0.02;
+        return (distance - 400) * 0.02; 
     } else if (distance > 2000 && distance <= 2500) {
-        return 25;
+        return 25; 
     } else {
-        return 'cannot travel that far';
+        return "cannot travel that far"; 
     }
-   } 
+}
+
+
+console.log(distanceFromHqInBlocks(43)); 
+console.log(distanceFromHqInFeet(50)); 
+console.log(distanceTravelledInFeet(43, 48)); 
+console.log(calculatesFarePrice(34, 32));
+console.log(calculatesFarePrice(10, 60)); 
